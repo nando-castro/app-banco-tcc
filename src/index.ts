9a7670c75931f9c4c3db3458faab2446de4b71f8
fernando <sousa.fernandocastro@gmail.com>
2023-07-20 14:51:47 -0300

    :sparkles: feat: novos métodos implementados

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,47 @@
-import prisma from "./database";
 import express, { json, Request, Response } from "express";
+import prisma from "./database";
 
 const app = express();
 app.use(json());
 
 app.get("/users", async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
-  res.sendStatus(200).send(users);
+  if (users.length > 0) return res.status(200).send(users);
+  return res.send("No users found");
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { name } = req.body;
+  await prisma.user.create({
+    data: {
+      name,
+    },
+  });
+  return res.sendStatus(201);
+});
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  await prisma.user.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  return res.sendStatus(200);
+});
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  await prisma.user.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      name,
+    },
+  });
+  return res.sendStatus(200);
 });
 
 const port = 5000;
